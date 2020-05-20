3077bf35699d0f24a1211a5d371e045f0c9a7503
Elliot Hohn <elliot@thefreshwatertrust.org>
2020-05-19 23:50:48 -0700

    Leaflet plot with animated circle markers for volcano eruptions

--- 2020-05-12_volcanos/server.R
+++ 2020-05-12_volcanos/server.R
@@ -1,40 +1,45 @@
 library(shiny)
 library(leaflet)
-library(leaflet.esri)
 library(sp)
 library(viridis)
 
-
 # load the dater
-volcano <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/volcano.csv')
 eruptions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/eruptions.csv')
-events <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/events.csv')
-
 coordinates(eruptions) <- ~longitude+latitude
 spdf <- SpatialPointsDataFrame(coords = eruptions@coords,
                                data = eruptions@data,
                                proj4string = CRS("+init=epsg:28992"))
 
 shinyServer(function(input, output, session) {
-  pal <- colorNumeric(palette = 'plasma', domain = spdf$vei)
+  pal <- colorNumeric(palette = 'inferno', domain = spdf$vei, reverse = TRUE)
+  
+  points <- reactive({
+    subset(spdf, start_year == input$year)
+    #spdf[spdf@data$start_year==input$year,]
+  })
   
   output$map <- renderLeaflet({
     leaflet() %>%
+      setView(lng = 23.434077, lat = 34.127021, zoom = 2) %>%
       addProviderTiles(providers$NASAGIBS.ViirsEarthAtNight2012) %>%
-      addCircleMarkers(data = spdf,
-                       radius = ~vei*4,
-                       color = 'transparent',
-                       fillColor = ~pal(vei),
-                       fillOpacity = 0.2
-                       ) %>%
       addLegend('topright',
                 pal = pal,
                 values = spdf[!is.na(spdf$vei),]$vei,
                 opacity = 1, 
-                title = 'Volcano\nExplosivity\nIndex')
+                title = 'Volcano</br>Explosivity</br>Index')
   })
   
+  observeEvent(points(), {
+    leafletProxy('map', data = points()) %>%
+      addCircleMarkers(data = points(),
+                       radius = ~vei*4,
+                       #color = 'transparent',
+                       color = ~pal(vei)
+                       #fillOpacity = 0.2
+      )
   }
+  )
+}
 )
 
  
--- 2020-05-12_volcanos/ui.R
+++ 2020-05-12_volcanos/ui.R
@@ -6,6 +6,27 @@ library(shinythemes)
 fluidPage(
   title = "Volcanos!",
   theme = shinytheme("slate"),
-  leafletOutput("map", width = "100%", height = '800px')
+  leafletOutput("map", width = "100%", height = '800px'),
+  absolutePanel(id = "title", #class = "panel panel-default", 
+                fixed = TRUE,
+                draggable = FALSE, 
+                top = 50, left = 50, right = 'auto', bottom = "auto",
+                width = 800,
+                h2('Volcanic Eruptions in last 2000 years')
+  ),
+  absolutePanel(id = "slider", #class = "panel panel-default", 
+                fixed = TRUE,
+                draggable = TRUE, 
+                right = 2, bottom = 20, top = 'auto', left = 'auto', 
+                width = 800,
+                sliderInput("year", "Eruption Year", 0, 
+                            2020,
+                            value = 2020,
+                            step = 100,
+                            animate = TRUE),
+                tags$script("$(document).ready(function(){
+                        setTimeout(function() {$('.slider-animate-button').click()},10);
+                    });")
+                )
 )
 
